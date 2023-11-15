97a8358886ba34924809199aa129c9e34619acd5
sjaworowicz <113131427+sjaworowicz@users.noreply.github.com>
2023-11-15 14:16:35 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let userName = "Silas";
+
+userName? console.log("Hello, Silas!") : console.log("Hello!");
+
+let userQuestion = "Will I become a pro gamer?"
+console.log(userQuestion)
+
+let randomNumber = Math.floor(Math.random() * 69);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    console.log(eightBall = "It is certain");
+    break;
+  case 1:
+    console.log(eightBall = "It is decidedly so");
+    break;
+  case 2:
+    console.log(eightBall = "Reply hazy try again");
+    break;
+  case 3:
+    console.log(eightBall = "Cannot predict now");
+    break;
+  case 4:
+    console.log(eightBall = "Do not count on it");
+    break;
+  case 5:
+    console.log(eightBall = "My sources say no");
+    break;
+  case 6:
+    consnole.log(eightBall = "Outlook not so good");
+    break;
+  case 7:
+    console.log(eightBall = "Signs point to yes");
+    break;
+}
